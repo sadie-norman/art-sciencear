1eb9fb2ece8b65a7cd4ed3ba7b6f6ce104cf04c7
sadie-norman <56067753+sadie-norman@users.noreply.github.com>
2019-10-28 15:38:44 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -77,7 +77,8 @@ function draw() {
     i++;
   }
 
-  // image(video, 0, 0, width, height);
+  image(video, 0, 0, width, height);
+  
   // We can call both functions to draw all keypoints and the skeletons
   drawKeypoints();
   // drawSkeleton();
