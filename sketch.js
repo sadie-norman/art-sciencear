bb9c13baefffc93ca78eac02ed0534e1201c1906
sadie-norman <56067753+sadie-norman@users.noreply.github.com>
2019-10-30 14:10:02 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -62,7 +62,7 @@ function setup() {
   //class prop threshold : number of boxes
   let options = {
     imageScaleFactor: 0.9,
-    flipHorizontal: true,
+    flipHorizontal: false,
     minConfidence: 10,
     maxPoseDetections: 3,
     scoreThreshold: 0.9,
